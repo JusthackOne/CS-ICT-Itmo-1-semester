997aa68916c27ee5f27350f58e81f3a6aa3526af
Михаил Недиков <nedikov.misha@yandex.ru>
2023-10-12 14:59:37 +0300

    add: some fixes_lab1

---
+++ tests/lab1/calculator.py
@@ -0,0 +1,24 @@
+def check(t):
+    t = t.replace(" ", "")
+    for k in ["**", "-", "+", "/", "*"]:
+        z = t.split(k)
+        if len(z) == 2:
+            break
+    if len(z) < 2:
+        print("Напишите строку в формате 2+2")
+        return False
+
+    for k in z:
+        if k.isalpha() is True:
+            print("Буквы не надо писать!")
+            return False
+
+    if z[-1] == "0" and "/" in t:
+        print("На ноль делить нельзя!")
+        return False
+    return True
+
+def calculator(t):
+    if check(t) is True:
+        return eval(t)
+    return 'Некоректные данные'
