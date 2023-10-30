c4cab9a305187d48b889c0e9da58c05303b78148
Михаил Недиков <nedikov.misha@yandex.ru>
2023-10-31 01:09:14 +0300

    Реализована функция поиска НОД

--- src/lab2/RSA.py
+++ src/lab2/RSA.py
@@ -1,3 +1,6 @@
+import random
+from typing import Tuple
+
 def is_prime(n: int) -> bool:
     """
     >>> is_prime(2)
@@ -7,7 +10,52 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    number = int(abs(n)**0.5)
+    number = int(abs(n)**0.5+1)
     return all([False for i in range(2, number) if n % i == 0])
 
+def gcd(a: int, b: int) -> int:
+    """
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
 
+    maximum_iter = 0
+    if a > b:
+        maximum_iter = b
+    else:
+        maximum_iter = a
+
+    maximalteiler = 0
+    for teiler in range(1, maximum_iter+1):
+        if a % teiler == 0 and b % teiler == 0:
+            maximalteiler = teiler
+
+    return maximalteiler
+
+
+def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+
+    n = p * q
+    phi = (p-1) * (q-1)
+    # PUT YOUR CODE HERE
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
